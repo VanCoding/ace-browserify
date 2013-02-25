b52c87254d0283bdf46fbaf80e99e1431b082ee7
Patrik Stutz <patrik.stutz@gmail.com>
2013-02-25 03:55:41 -0600

    init

---
+++ build.js
@@ -0,0 +1,10 @@
+var cp = require("child_process");
+var fs = require("fs");
+
+cp.exec("git clone git://github.com/ajaxorg/ace-builds.git \""+__dirname+"/ace\"",function(){        
+    var files = fs.readdirSync(__dirname+"/ace/src-min-noconflict/");
+    for(var i = 0; i < files.length; i++){
+        fs.writeFile(__dirname+"/"+files[i],fs.readFileSync(__dirname+"/ace/src-min-noconflict/"+files[i]))
+    }    
+    console.log("done");
+});
